b62fa28acefa42bb1d32b1ad581feaada8fc279f
ra2dd <98891234+ra2dd@users.noreply.github.com>
2023-04-12 17:36:47 +0200

    building elements and objects

--- main.js
+++ main.js
@@ -1,1 +1,69 @@
-console.log('dziala');
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+const todoButton = document.querySelector('button');
+
+function buildUniqueId(prefix = 'prefix')
+{
+    return `${prefix}-${Math.floor(Math.random() * Date.now())}`;
+}
+
+const state = 
+{
+    taksName: "",
+    tasks: 
+    [
+        {
+            id: "todo-0",
+            name: "Learn javascript"
+        }
+    ]
+};
+
+function returnTaskObject(name)
+{
+    /*
+    state.tasks.push
+    (
+        {
+            id: buildUniqueId('todo'),
+            name
+        }
+    );
+    */
+    return
+    {
+        id: buildUniqueId('todo'),
+        name
+    };
+}
+
+function buildTodoElement(id, name)
+{
+    const listItem = document.createElement('li');
+    const span = document.createElement('span');
+    const liTextContent = document.createTextNode('name');
+
+    span.appendChild(liTextContent);
+    listItem.id = id;
+    listItem.appendChild(span);
+    listItem.appendChild(buildDeleteButton());
+
+    return listItem;
+}
+
+function buildDeleteButton()
+{
+    const button = document.createElement('button');
+    const deleteTextContent = document.createTextNode('Detele');
+
+    button.setAttribute('type', 'button');
+    button.textContent = deleteTextContent;
+    button.addEventListener('click', (event) =>
+    {
+        event.target.remove();
+    });
+
+    return button;
+}
+
